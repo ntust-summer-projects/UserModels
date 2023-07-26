34baa489a2a1b5058aa28b0cf53c689ea04af29d
nashi999 <103909693+nashi999@users.noreply.github.com>
2023-07-26 10:47:43 +0800

    Update models.py

--- RoleCreation/main/models.py
+++ RoleCreation/main/models.py
@@ -7,77 +7,51 @@ from django.db.models.query import QuerySet
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 # Create your models here.
+class NormalManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        result = super().get_queryset(*args,**kwargs).filter(role=User.Role.NORMAL)
+        return result
+class CompanyManager(BaseUserManager):
+    def get_queryset(self,*args,**kwargs):
+        result = super().get_queryset(*args,**kwargs).filter(role = User.Role.COMPANY)
+        return result
 class User(AbstractUser):
     class Role(models.TextChoices):
         ADMIN = "ADMIN", 'Admin'
         NORMAL = "NORMAL", 'Normal'
         COMPANY = "COMPANY", 'Company'
         
-    base_role = Role.ADMIN
+    # base_role = Role.ADMIN
     
     phone = models.CharField(max_length=50,null=True,blank=True)
     
     role = models.CharField(max_length=50, choices=Role.choices,default = Role.ADMIN)
     
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.role = self.base_role
-            return super().save(*args,**kwargs)
+    company = CompanyManager()
+    
+    normal = NormalManager()
  
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created and instance.role == "ADMIN":
+    if created and instance.role == "ADMIN" or 'Admin':
         Profile.objects.bulk_create(
             [Profile(user=instance,meta_key="name"),
              Profile(user=instance,meta_key="IsAdmin",meta_value="true")]
             ) 
- 
-class NormalManager(BaseUserManager):
-    def get_queryset(self,*args,**kwargs):
-        result = super().get_queryset(*args,**kwargs).filter(role=User.Role.NORMAL)
-        return result
-        
-class Normal(User):
-    
-    base_role = User.Role.NORMAL
-    
-    normal = NormalManager()
-    
-    class Meta:
-        proxy = True
-
-@receiver(post_save, sender=Normal)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created and instance.role == "NORMAL":
+    elif created and instance.role == "NORMAL" or 'Normal':
         Profile.objects.bulk_create(
             [Profile(user=instance,meta_key="name"),
              Profile(user=instance,meta_key="wallet",meta_value=0),
              Profile(user=instance,meta_key="carbonProduce",meta_value=0.0000)]
             ) 
-
-class CompanyManager(BaseUserManager):
-    def get_queryset(self,*args,**kwargs):
-        result = super().get_queryset(*args,**kwargs).filter(role = User.Role.COMPANY)
-        return result
-        
-class Company(User):
-    
-    base_role = User.Role.COMPANY
-    
-    company = CompanyManager()
-    
-    class Meta:
-        proxy = True
-
-@receiver(post_save, sender=Company)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created and instance.role == "COMPANY":
+    elif created and instance.role == "COMPANY" or 'Company':
         Profile.objects.bulk_create(
             [Profile(user=instance,meta_key="companyName"),
              Profile(user=instance,meta_key="address"),
              Profile(user=instance,meta_key="vatNumber"),
              Profile(user=instance,meta_key="chairman")]
             )
+ 
 
 class Profile(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
